87754d41d5abe097ff4e9b1d80ca4586da6d96ea
Ashwin Vinod <ashwinvinodsa@gmail.com>
2023-07-18 13:12:29 +0530

    Polished the UI

--- app.py
+++ app.py
@@ -37,19 +37,20 @@ class InputFrame(tk.Frame):
         super().__init__(master)
         self.pack(fill=tk.BOTH, side=tk.TOP)
         self.tabbed_window = ttk.Notebook(self)
-        self.tabbed_window.pack(padx=5, pady=5, expand=True, fill=tk.BOTH)
+        self.tabbed_window.pack(expand=True, fill=tk.BOTH)
         self.show_ui()
 
     def show_ui(self):
-        self.e_search_frame = EmployeeSearchFrame(self.tabbed_window)
+        self.e_search_frame = EmployeeMainFrame(self.tabbed_window)
         self.e_search_frame.pack(fill=tk.BOTH, expand=True)
 
-        self.tabbed_window.add(self.e_search_frame, text="Employee Search")
+        self.tabbed_window.add(self.e_search_frame, text="Employee Management")
 
 
-class EmployeeSearchFrame(tk.Frame):
+class EmployeeMainFrame(tk.Frame):
     def __init__(self, master=None) -> None:
         super().__init__(master)
+        
         self.pack(fill=tk.BOTH)
         self.employee_id = tk.StringVar()
         self.employee_f_name = tk.StringVar()
@@ -59,40 +60,38 @@ class EmployeeSearchFrame(tk.Frame):
         self.show_ui()
 
     def show_ui(self):
-        ttk.Style(self).configure("S.TLabel.grid", sticky="NSWE")
-        ttk.Style(self).configure(
-            "S.TEntry",
-            border=(3, 3, 3, 3),
-            sticky="NSWE",
-            justify=tk.LEFT,
-        )
+        ttk.Style(self).configure("TLabel", bg=self["background"])
+        id_label = ttk.Label(self, text="Employee ID:")
+        id_label.grid_configure(row=0, column=0, sticky="nswe", ipadx=5, ipady=3)
+        id_entry = ttk.Entry(self, text="Enter ID", textvariable=self.employee_id)
+        id_entry.grid_configure(row=0, column=1, sticky="nswe", ipadx=5, ipady=3)
 
-        id_label = ttk.Label(self, text="Employee ID:", style="S.TLabel").grid(row=0, column=0)
-        id_entry = ttk.Entry(self, style="S.TEntry", text="Enter ID", textvariable=self.employee_id).grid(
-            row=0, column=1
-        )
+        f_name_label = ttk.Label(self, text="First Name:")
+        f_name_label.grid_configure(row=1, column=0, sticky="nswe", ipadx=5, ipady=3)
+        f_name_entry = ttk.Entry(self, text="Enter First Name", textvariable=self.employee_f_name)
+        f_name_entry.grid_configure(row=1, column=1, sticky="nswe", ipadx=5, ipady=3)
 
-        f_name_label = ttk.Label(self, text="First Name:", style="S.TLabel").grid(row=1, column=0)
-        f_name_entry = ttk.Entry(
-            self, style="S.TEntry", text="Enter First Name", textvariable=self.employee_f_name
-        ).grid(row=1, column=1)
+        l_name_label = ttk.Label(self, text="Last Name:")
+        l_name_label.grid_configure(row=2, column=0, sticky="nswe", ipadx=5, ipady=3)
+        l_name_entry = ttk.Entry(self, text="Enter Last Name", textvariable=self.employee_l_name)
+        l_name_entry.grid_configure(row=2, column=1, sticky="nswe", ipadx=5, ipady=3)
 
-        l_name_label = ttk.Label(self, text="Last Name:", style="S.TLabel").grid(row=2, column=0)
-        l_name_entry = ttk.Entry(
-            self, style="S.TEntry", text="Enter Last Name", textvariable=self.employee_l_name
-        ).grid(row=2, column=1)
+        ph_label = ttk.Label(self, text="Ph no:")
+        ph_label.grid_configure(row=0, column=2, sticky="nswe", ipadx=5, ipady=3)
+        ph_entry = ttk.Entry(self, text="Enter phone number", textvariable=self.employee_ph)
+        ph_entry.grid_configure(row=0, column=3, sticky="nswe", ipadx=5, ipady=3)
 
-        ph_label = ttk.Label(self, text="Ph no:", style="S.TLabel").grid(row=0, column=2, sticky="NSWE")
-        ph_entry = ttk.Entry(self, style="S.TEntry", text="Enter phone number", textvariable=self.employee_ph).grid(
-            row=0, column=3
-        )
+        address_label = ttk.Label(self, text="Address: ")
+        address_label.grid_configure(row=1, column=2, sticky="nswe", ipadx=5, ipady=3)
+        address_entry = ttk.Entry(self, text="Enter Address", textvariable=self.employee_address)
+        address_entry.grid_configure(row=1, column=3, sticky="nswe", ipadx=5, ipady=3)
 
-        address_label = ttk.Label(self, text="Address: ", style="S.TLabel").grid(row=1, column=2)
-        address_entry = ttk.Entry(
-            self, style="S.TEntry", text="Enter Address", textvariable=self.employee_address
-        ).grid(row=1, column=3)
+        search_button = ttk.Button(self, text="Search", width = 5)
+        search_button.grid_configure(row=2, column=3, sticky="nse", ipadx=5, ipady=3)
+        
+        add_employee_button = ttk.Button(self, text="Add", width=5)
+        add_employee_button.grid_configure(row=2, column=3, sticky="nsw", ipadx=5, ipady=3)
 
-        search_button = ttk.Button(self, text="search").grid(row=2, column=3)
 
 if __name__ == "__main__":
     root = tk.Tk()
